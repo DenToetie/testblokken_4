30a0b2527bb69b23bb9a4af314aa9667ca101037
Guido Geurs <57041484+DenToetie@users.noreply.github.com>
2024-10-08 09:13:18 +0200

    Comment block added.

---
+++ pijlen.ts
@@ -0,0 +1,26 @@
+
+/**
+* Gebruik dit bestand om specifieke functies en blokken te definiëren.
+* Lees meer op https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace Tekst {
+    /**
+     * block with comment
+     * @param s tekst
+     */
+    //% block
+    export function Comment(s: string): void {
+        // Add code here
+    }
+
+}
